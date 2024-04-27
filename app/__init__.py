226e88314ed1d85dfe8dca570dc0a08d146b6752
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-04-27 14:02:06 -0300

    feat: configurando create_app para iniciar projeto

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_migrate import Migrate
+from app.models import db, Department, Employee, Dependent  # Agora este import deve funcionar corretamente
+from app.routes import routes_blueprint
+from config import DevelopmentConfig, ProductionConfig, TestingConfig
+import os
+
+def create_app():
+    app = Flask(__name__)
+
+    if os.environ.get('FLASK_ENV') == 'production':
+        env_config = ProductionConfig
+    elif os.environ.get('FLASK_ENV') == 'test':
+        env_config = TestingConfig
+    else:
+        env_config = DevelopmentConfig
+
+    app.config.from_object(env_config)
+    db.init_app(app)
+
+    migrate = Migrate(app, db)
+    
+    app.register_blueprint(routes_blueprint)
+
+    return app
