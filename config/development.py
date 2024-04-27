d986c0a7c665ffd97d8bec0439febc6969bb98f0
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-04-27 11:44:19 -0300

    feat: setando config de ambientes de projeto

--- config/development.py
+++ config/development.py
@@ -1,3 +1,6 @@
-DEBUG = True
-SECRET_KEY = '9ba263503b01ce2ef81f6641f504b45333aa0662183d0184db79d9e92ccef620'
-# outras configurações específicas de desenvolvimento
+class DevelopmentConfig:
+    DEBUG = True
+    # lembrar de alocar a secret key no .env
+    SECRET_KEY = '9ba263503b01ce2ef81f6641f504b45333aa0662183d0184db79d9e92ccef620'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@localhost/development_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- config/production.py
+++ config/production.py
@@ -1,3 +1,5 @@
-DEBUG = False
-SECRET_KEY = '9ba263503b01ce2ef81f6641f504b45333aa0662183d0184db79d9e92ccef620'
-# outras configurações específicas de produção
+class ProductionConfig:
+    DEBUG = False
+    SECRET_KEY = '9ba263503b01ce2ef81f6641f504b45333aa0662183d0184db79d9e92ccef620'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://username:password@hostname/production_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
