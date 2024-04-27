444996711099bcee135b08099176fbd5418f06df
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-04-27 14:01:00 -0300

    feat: setando nomes das tabelas

--- app/models/models.py
+++ app/models/models.py
@@ -1,17 +1,23 @@
-db = 'teste'
+from . import db
 
 class Department(db.Model):
+    __tablename__ = "department"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     employees = db.relationship('Employee', backref='department', lazy=True)
 
 class Employee(db.Model):
+    __tablename__ = "employee"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     department_id = db.Column(db.Integer, db.ForeignKey('department.id'), nullable=False)
     dependents = db.relationship('Dependent', backref='employee', lazy=True)
 
 class Dependent(db.Model):
+    __tablename__ = "dependent"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+
+
+
