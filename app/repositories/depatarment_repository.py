cc7695d19b8b8dde332df6fad0a4c10f8bd7ae9e
TiagoOliverDev <paulloholiveira77@gmail.com>
2024-04-29 11:16:26 -0300

    feat: inserindo comentários nas funções

--- app/repositories/depatarment_repository.py
+++ app/repositories/depatarment_repository.py
@@ -8,6 +8,19 @@ class DepartamentRepository():
         self.db = db
 
     def create_department(self, name: str):
+        """
+        Cria um novo departamento no banco de dados.
+
+        Esta função tenta criar um departamento com o nome fornecido. Se a operação
+        de inserção no banco de dados for bem-sucedida, ela retorna o ID do novo
+        departamento criado. Em caso de falha, faz o rollback da transação e loga o erro.
+
+        Args:
+            name (str): O nome do departamento a ser criado.
+
+        Returns:
+            int or None: Retorna o ID do departamento criado se bem-sucedido; None se houver falha.
+        """
         try:
             new_department = Department(name=name)
             self.db.session.add(new_department)
@@ -23,6 +36,17 @@ class DepartamentRepository():
         return Department.query.filter_by(name=name).first() is not None
         
     def list_departments(self):
+        """
+        Lista todos os departamentos ordenados pelo ID.
+
+        Esta função recupera todos os departamentos do banco de dados, ordenados pelo
+        ID de forma ascendente. Em caso de falha na consulta ao banco, loga o erro e
+        retorna uma lista vazia.
+
+        Returns:
+            list: Uma lista de objetos Department se a consulta for bem-sucedida,
+                ou uma lista vazia se houver falha na consulta.
+        """
         try:
             departments = Department.query.order_by(Department.id).all()
             return departments
@@ -31,6 +55,21 @@ class DepartamentRepository():
             return []
         
     def update_department(self, department_id: int, new_name: str):
+        """
+        Atualiza o nome de um departamento existente.
+
+        Esta função tenta encontrar um departamento pelo seu ID e atualizar seu nome.
+        Se o departamento for encontrado e atualizado com sucesso, retorna True.
+        Caso o departamento não seja encontrado ou haja algum erro na transação,
+        faz rollback e retorna False.
+
+        Args:
+            department_id (int): O ID do departamento a ser atualizado.
+            new_name (str): O novo nome a ser atribuído ao departamento.
+
+        Returns:
+            bool: True se o departamento for atualizado com sucesso, False caso contrário.
+        """
         try:
             department = Department.query.get(department_id)
             if department:
@@ -44,6 +83,20 @@ class DepartamentRepository():
             return False
         
     def delete_department(self, department_id: int):
+        """
+        Exclui um departamento específico pelo ID.
+
+        Tenta encontrar e excluir um departamento com base no ID fornecido. Se o departamento
+        for encontrado e excluído com sucesso, confirma a transação e retorna True.
+        Se o departamento não for encontrado, ou se ocorrer um erro durante a transação,
+        realiza rollback e retorna False.
+
+        Args:
+            department_id (int): O ID do departamento a ser excluído.
+
+        Returns:
+            bool: True se o departamento for excluído com sucesso, False caso contrário.
+        """
         try:
             department = Department.query.get(department_id)
             if department:
@@ -57,6 +110,18 @@ class DepartamentRepository():
             return False
         
     def get_department_by_id(self, department_id: int):
+        """
+        Busca um departamento pelo seu ID.
+
+        Realiza uma consulta ao banco de dados para encontrar um departamento pelo ID especificado.
+        Retorna o objeto Department se encontrado, ou None se não houver departamento com o ID fornecido.
+
+        Args:
+            department_id (int): O ID do departamento a ser buscado.
+
+        Returns:
+            Department or None: O objeto Department se encontrado, None se não houver departamento correspondente.
+        """
         try:
             department = Department.query.get(department_id)
             if department:
--- app/repositories/employee_repository.py
+++ app/repositories/employee_repository.py
@@ -10,7 +10,22 @@ class EmployeeRepository():
         self.db = db
 
     def create_employee(self, name: str, department_id: int, dependents=None):
-        """Adiciona um novo colaborador ao banco de dados, junto com seus dependentes, se houver."""
+        """
+        Adiciona um novo colaborador ao banco de dados e, opcionalmente, seus dependentes.
+
+        Esta função cria um novo colaborador com o nome e o ID do departamento fornecidos.
+        Se houver dependentes, eles também serão adicionados ao banco de dados. A função
+        tenta salvar todas as informações no banco de dados e, se bem-sucedida, retorna o ID
+        do novo colaborador. Se ocorrer uma falha durante a transação, realiza um rollback.
+
+        Args:
+            name (str): O nome do colaborador.
+            department_id (int): O ID do departamento ao qual o colaborador pertencerá.
+            dependents (list of str, optional): Uma lista de nomes de dependentes do colaborador.
+
+        Returns:
+            int or None: Retorna o ID do novo colaborador se bem-sucedido; None em caso de falha.
+        """
         try:
             new_employee = Employee(name=name, department_id=department_id)
             self.db.session.add(new_employee)
@@ -29,7 +44,21 @@ class EmployeeRepository():
             return None  
 
     def get_employees_by_department(self, department_id: int):
-        """Retorna todos os colaboradores de um departamento específico, com a flag indicando se possuem dependentes."""
+        """
+        Retorna uma lista de colaboradores de um determinado departamento, indicando se têm dependentes.
+
+        Busca no banco de dados todos os colaboradores de um departamento específico, junto com a contagem
+        de seus dependentes. Retorna uma lista de dicionários com detalhes dos colaboradores e uma flag 
+        booleana indicando a presença de dependentes.
+
+        Args:
+            department_id (int): ID do departamento do qual se deseja listar colaboradores.
+
+        Returns:
+            list of dict: Uma lista contendo dicionários com informações dos colaboradores e uma flag 
+                        'have_dependents' para indicar se possuem dependentes. Retorna uma lista vazia
+                        se nenhum colaborador for encontrado.
+        """
         try:
             employees = (self.db.session.query(Employee, func.count(Dependent.id).label('dependents_count'))
                         .outerjoin(Dependent, Employee.id == Dependent.employee_id)
@@ -52,6 +81,28 @@ class EmployeeRepository():
         return Employee.query.filter_by(name=name).first() is not None
         
     def update_employee(self, employee_id: int, new_name: str = None, new_department_id: int = None, new_dependents: list = None):
+        """
+        Atualiza os dados de um colaborador existente no banco de dados.
+
+        Esta função permite atualizar o nome, o departamento e/ou os dependentes de um colaborador específico.
+        Se algum dos parâmetros é fornecido, a função aplica as alterações correspondentes. A atualização dos
+        dependentes envolve remover todos os dependentes atuais e adicionar os novos fornecidos na lista.
+
+        Args:
+            employee_id (int): O ID do colaborador cujos dados serão atualizados.
+            new_name (str, optional): O novo nome do colaborador, se uma alteração for necessária.
+            new_department_id (int, optional): O novo ID do departamento, se uma mudança for necessária.
+            new_dependents (list of str, optional): Uma nova lista de nomes de dependentes para substituir
+                                                    os atuais, se uma mudança for necessária.
+
+        Returns:
+            bool: True se a atualização for bem-sucedida, False se falhar devido a um erro ou se o colaborador
+                não for encontrado.
+
+        Raises:
+            Exception: Captura e loga qualquer exceção que ocorra durante a operação de atualização,
+                    realizando rollback na transação para manter a integridade dos dados.
+        """
         try:
             employee = Employee.query.get(employee_id)
             if not employee:
@@ -84,6 +135,22 @@ class EmployeeRepository():
         return employee is not None
     
     def delete_employee(self, employee_id: int):
+        """
+        Exclui um colaborador existente do banco de dados.
+
+        Busca o colaborador pelo ID fornecido e, se encontrado, remove-o do banco de dados. Se a operação
+        de exclusão for bem-sucedida, confirma a transação e retorna True. Se o colaborador não for encontrado,
+        ou se ocorrer algum erro durante a exclusão, a função retorna False.
+
+        Args:
+            employee_id (int): O ID do colaborador a ser excluído.
+
+        Returns:
+            bool: True se o colaborador for excluído com sucesso, False caso contrário.
+
+        Raises:
+            Exception: Loga e retorna False em caso de qualquer exceção, garantindo que nenhum dado parcial seja salvo.
+        """
         try:
             employee = Employee.query.get(employee_id)
             if employee:
@@ -97,6 +164,23 @@ class EmployeeRepository():
             return False
         
     def get_employee_by_id(self, employee_id: int):
+        """
+        Recupera um colaborador pelo seu ID, incluindo informações sobre seu departamento e dependentes.
+
+        A função busca no banco de dados o colaborador especificado pelo ID, incluindo detalhes de seus dependentes
+        e o departamento ao qual pertence. Retorna um dicionário com os dados do colaborador se encontrado.
+        Retorna None se o colaborador não for encontrado ou em caso de erro na consulta.
+
+        Args:
+            employee_id (int): O ID do colaborador a ser buscado.
+
+        Returns:
+            dict or None: Um dicionário contendo informações do colaborador, seu departamento e dependentes, ou None
+                        se não for encontrado ou em caso de erro.
+
+        Raises:
+            Exception: Loga um erro se ocorrer uma exceção durante a busca.
+        """
         try:
             employee = (Employee.query
                         .options(joinedload(Employee.department), joinedload(Employee.dependents))
